4df96cad55487587ca099cf97eef035bdb8f3240
Jorge David Runza Gualdron <jd.runza@uniandes.edu.co>
2016-07-18 01:12:36 -0500

    Cambio para recibir 2 parametros

--- gondola/gondola/arduino.py
+++ gondola/gondola/arduino.py
@@ -2,19 +2,22 @@ from django.http import HttpResponse
 import serial
 import json
 
-def sendPrice(request, value):
+
+def sendPrice(request, value, price):
     if request.method == 'GET':
         try:
-            price = value
+            prod = str(value)
+            pr = str(price)
+            blanks = "    "
+            result = value + blanks + pr
             port = "COM2"
             baud = 9600
             ser = serial.Serial(port, baud, timeout=1)
             if ser.isOpen():
-             ser.write(price.encode('ascii')+'\r\n')
-             ser.close()
-             return HttpResponse(json.dumps({'ack':'1'}), content_type="application/json")
+                ser.write(result.encode('ascii') + '\r\n')
+                ser.close()
+                return HttpResponse(json.dumps({'ack': '1'}), content_type="application/json")
             else:
-             return HttpResponse(json.dumps({'ack':'0'}), content_type="application/json", status=400)
+                return HttpResponse(json.dumps({'ack': '0'}), content_type="application/json", status=400)
         except Exception as e:
             return HttpResponse(json.dumps(e.args), content_type="application/json", status=400)
-
